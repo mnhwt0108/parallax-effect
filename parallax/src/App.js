5989e7e624534474098bc40f4d584ff3df68a82c
Minh Nhựt <minhnhutvn02@gmail.com>
2022-08-23 14:22:37 +0700

    Update

--- parallax/src/App.js
+++ parallax/src/App.js
@@ -5,13 +5,14 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-
-      
+      <div className="Navbar">
+        <Navbar/>
+      </div>
     <section>
       <img src={require('./images/stars.png')} id="stars" alt='stars' />
       <img src={require('./images/moon.png')} id="moon" alt='moon' />
       <img src={require('./images/mountains_behind.png')} id="mountains_behind" alt='mountains_behind' />
+      <h2 id='text'> Moon Light </h2>
       <img src={require('./images/mountains_front.png')} id="mountains_front" alt='mountains_front' />
     </section>
     </div>
