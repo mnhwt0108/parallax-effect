4012bf8ab7432f03c559b17294447a2f050e520d
Minh Nhựt <minhnhutvn02@gmail.com>
2022-08-23 15:58:48 +0700

    Update App.js

--- parallax/src/App.js
+++ parallax/src/App.js
@@ -15,7 +15,7 @@ function App() {
     moon.style.top = value * 1.05 + "px";
     mountains_behind.style.top = value * 0.5 + "px";
     mountains_front.style.top = value * 0.5 + "px";
-    text.style.marginRight = value * 4 + "px";
+    text.style.marginRight = value * 2 + "px";
     text.style.marginTop = value * 1.5 + "px";
     btn.style.marginTop = value * 1.5 + "px";
   });
